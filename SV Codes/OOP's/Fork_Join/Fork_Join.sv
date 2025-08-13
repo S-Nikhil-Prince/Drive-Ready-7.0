db11da000f3c61f68c6b01c00fae166d25fcc169
Nikitha <nikithasodadasi07@gmail.com>
2025-08-13 11:54:42 +0530

    Changes to be committed: 	modified:   SV Codes/OOP's/Fork_Join/Fork_Join.sv 	modified:   SV Codes/OOP's/Fork_Join/Output.v

--- SV Codes/OOP's/Fork_Join/Fork_Join.sv
+++ SV Codes/OOP's/Fork_Join/Fork_Join.sv
@@ -12,9 +12,11 @@ module tb;
     ->done;
   end
   initial begin
-    #10;
-    data2=data1;
-    $display("recived Testcase = %0d",data2);
+    forever begin
+      #10;
+      data2=data1;
+      $display("Recived Testcase = %0d",data2);
+    end
   end
   initial begin
     wait(done.triggered);
@@ -23,39 +25,44 @@ module tb;
 endmodule
 // Using more number of initail begin statements leads to race condition
 // and the output will be unpredictable.
-//so we will use "Fork_Join"
+//so we will use "Fork_Join".
 
 //=-=-=-=- CODE -=-=-=-=-=
 //-_-_-_-_-_Fork_Join_-_-_-_-_-
 
-// module tb;
-//   int data1,data2;
-//   event done;
-//   initial begin
-//     fork
-//       generator();    //Fork and Join Declaration
-//       driver();
-//       waiter();
-//     join
-//   end
-//   task generator();
-//     for(int i=0;i<8;i++) begin
-//       data1=$random();
-//       $display("Generated Test case = %0d",data1);
-//       #1;
-//       #9;
-//     end
-//     ->done;
-//   endtask
-//   task driver();
-//     forever begin
-//       #10;
-//       data2=data1;
-//       $display("recived Testcase = %0d",data2);
-//     end
-//   endtask
-//   task waiter();
-//     wait(done.triggered);
-//     $finish;
-//   endtask
-// endmodule
+module tb;
+  int data1,data2;
+  event done;
+  initial begin
+    fork
+      generator();    //Fork and Join Declaration
+      driver();
+      waiter();
+    join
+  end
+  task generator();
+    for(int i=0;i<8;i++) begin
+      data1=$random();
+      $display("Generated Test case = %0d",data1);
+      #1;
+      #9;
+    end
+    ->done;
+  endtask
+  task driver();
+    forever begin
+      #10;
+      data2=data1;
+      $display("recived Testcase = %0d",data2);
+    end
+  endtask
+  task waiter();
+    wait(done.triggered);
+    $finish;
+  endtask
+endmodule
+
+///types of Fork_Join
+// 1. Fork_Join
+// 2. Fork_Join_any
+// 3. Fork_Join_none
