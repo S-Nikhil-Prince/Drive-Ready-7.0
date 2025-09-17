5398f6136d36fd00a3eeef2d5acaa0aafa486c45
S-Nikhil-Prince <23a91a0459@aec.edu.in>
2025-09-17 12:08:57 +0530

    Changes to be committed: 	new file:   SV Notes/Linux Notes/notes.v/notes.v 	modified:   SV Notes/OOP's/mas_slv_tx.sv 	new file:   SV Notes/linux notes.md

--- SV Notes/OOP's/mas_slv_tx.sv
+++ SV Notes/OOP's/mas_slv_tx.sv
@@ -97,3 +97,277 @@ Printing Salve Values                                       |
 TX    : wr_rd=1 | sel=4 | addr=5a | data=12345678           |   
 SLAVE : wr_rd=1 | sel=4 | addr=5a | data=12345678           |
 ------------------------------------------------------------
+===============================================================================
+Version 2 {Full APB With 4 Slave and Proper Logic}
+===============================================================================
+//Transmission Class
+class apb_tx;
+  bit wr_rd;
+  bit [3:0] sel;
+  bit [31:0] data;
+  bit [7:0] addr;
+
+  function void print();
+    $display("TX    : wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+//Master Class
+class apb_mas;
+  rand bit wr_rd;
+  rand bit [3:0] sel;
+  rand bit [31:0] data;
+  rand bit [7:0] addr;
+  
+  constraint sel_c 
+  {
+    sel inside {4'b0001,4'b0010,4'b0100,4'b1000};
+  }
+  
+  function void transmit(apb_tx tx);
+    tx.wr_rd = this.wr_rd;
+    tx.sel   = this.sel;
+    tx.data  = this.data;
+    tx.addr  = this.addr;
+  endfunction
+  
+  function void recive(apb_tx tx);
+    this.wr_rd = tx.wr_rd;
+    this.addr  = tx.addr;
+    this.data  = tx.data;
+    this.sel = tx.sel;
+  endfunction
+
+  function void print();
+    $display("MASTER: wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+//Slave Clasd
+class apb_slv1;
+  bit wr_rd;
+  bit [3:0] sel;
+  bit [7:0] addr;
+  bit [31:0] data;
+
+  function void recive(apb_tx tx);
+    this.wr_rd = tx.wr_rd;
+    this.addr  = tx.addr;
+    this.data  = tx.data;
+    this.sel = tx.sel;
+  endfunction
+  
+  function void transmit(apb_tx tx);
+    tx.wr_rd = this.wr_rd;
+    tx.sel   = this.sel;
+    tx.data  = this.data;
+    tx.addr  = this.addr;
+  endfunction
+
+  function void print();
+    $display("SLAVE : wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+class apb_slv2;
+  bit wr_rd;
+  bit [3:0] sel;
+  bit [7:0] addr;
+  bit [31:0] data;
+
+  function void recive(apb_tx tx);
+    this.wr_rd = tx.wr_rd;
+    this.addr  = tx.addr;
+    this.data  = tx.data;
+    this.sel = tx.sel;
+  endfunction
+  
+  function void transmit(apb_tx tx);
+    tx.wr_rd = this.wr_rd;
+    tx.sel   = this.sel;
+    tx.data  = this.data;
+    tx.addr  = this.addr;
+  endfunction
+
+  function void print();
+    $display("SLAVE : wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+class apb_slv3;
+  bit wr_rd;
+  bit [3:0] sel;
+  bit [7:0] addr;
+  bit [31:0] data;
+
+  function void recive(apb_tx tx);
+    this.wr_rd = tx.wr_rd;
+    this.addr  = tx.addr;
+    this.data  = tx.data;
+    this.sel = tx.sel;
+  endfunction
+  
+  function void transmit(apb_tx tx);
+    tx.wr_rd = this.wr_rd;
+    tx.sel   = this.sel;
+    tx.data  = this.data;
+    tx.addr  = this.addr;
+  endfunction
+
+  function void print();
+    $display("SLAVE : wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+class apb_slv4;
+  bit wr_rd;
+  bit [3:0] sel;
+  bit [7:0] addr;
+  bit [31:0] data;
+
+  function void recive(apb_tx tx);
+    this.wr_rd = tx.wr_rd;
+    this.addr  = tx.addr;
+    this.data  = tx.data;
+    this.sel = tx.sel;
+  endfunction
+  
+  function void transmit(apb_tx tx);
+    tx.wr_rd = this.wr_rd;
+    tx.sel   = this.sel;
+    tx.data  = this.data;
+    tx.addr  = this.addr;
+  endfunction
+
+  function void print();
+    $display("SLAVE : wr_rd=%0d | sel=%0b | addr=%0h | data=%0h",wr_rd,sel,addr,data);
+  endfunction
+endclass
+
+module tb;
+  apb_mas mas;
+  apb_tx  tx;
+  apb_slv1 slv1;
+  apb_slv2 slv2;
+  apb_slv3 slv3;
+  apb_slv4 slv4;
+  initial begin
+    mas = new();
+    tx  = new();
+    slv1 = new();
+    slv2 = new();
+    slv3 = new();
+    slv4 = new();
+    repeat(20) begin 
+      mas.randomize();
+      if (mas.wr_rd==1) begin
+        if(mas.sel==4'b0001) begin
+          $display("Master Values");
+          mas.print();
+          $display("Write Operation {wr_rd = 1}");
+          $display("Slave 1 Selected {sel=4'b0001}");
+          mas.transmit(tx);
+          $display("Master Transmit Data into the Tx");
+          tx.print();
+          slv1.recive(tx);
+          $display("Slave Recived Data");
+          slv1.print();
+          $display("Write Transaction Completed");
+          $display("===========================");
+        end
+        if(mas.sel==4'b0010) begin
+          $display("Master Values");
+          mas.print();
+          $display("Write Operation {wr_rd = 1}");
+          $display("Slave 2 Selected {sel=4'b0010}");
+          mas.transmit(tx);
+          $display("Master Transmit Data into the Tx");
+          tx.print();
+          slv2.recive(tx);
+          $display("Slave2 Recived Data");
+          slv2.print();
+          $display("Write Transaction Completed");
+          $display("===========================");
+        end
+        if(mas.sel==4'b0100) begin
+          $display("Master Values");
+          mas.print();
+          $display("Write Operation {wr_rd = 1}");
+          $display("Slave 3 Selected {sel=4'b0100}");
+          mas.transmit(tx);
+          $display("Master Transmit Data into the Tx");
+          tx.print();
+          slv3.recive(tx);
+          $display("Slave3 Recived Data");
+          slv3.print();
+          $display("Write Transaction Completed");
+          $display("===========================");
+        end
+        if(mas.sel==4'b1000) begin
+          $display("Master Values");
+          mas.print();
+          $display("Write Operation {wr_rd = 1}");
+          $display("Slave 4 Selected {sel=4'b1000}");
+          mas.transmit(tx);
+          $display("Master Transmit Data into the Tx");
+          tx.print();
+          slv4.recive(tx);
+          $display("Slave4 Recived Data");
+          slv4.print();
+          $display("Write Transaction Completed");
+          $display("===========================");
+        end
+      end
+      else begin
+        if(mas.sel==4'b0001) begin
+          $display("Read Operation {wr_rd = 0}");
+          $display("Slave 1 Selected {sel=4'b0001}");
+          slv1.transmit(tx);
+          $display("Slave Transimit Data into Tx");
+          tx.print();
+          mas.recive(tx);
+          $display("Master Recived Data");
+          mas.print();
+          $display("Read Transaction Competed");
+          $display("==========================");
+        end
+        if(mas.sel==4'b0010) begin
+          $display("Read Operation {wr_rd = 0}");
+          $display("Slave 2 Selected {sel=4'b0010}");
+          slv2.transmit(tx);
+          $display("Slave Transimit Data into Tx");
+          tx.print();
+          mas.recive(tx);
+          $display("Master Recived Data");
+          mas.print();
+          $display("Read Transaction Competed");
+          $display("==========================");
+        end
+        if(mas.sel==4'b0100) begin
+          $display("Read Operation {wr_rd = 0}");
+          $display("Slave 3 Selected {sel=4'b0100}");
+          slv3.transmit(tx);
+          $display("Slave Transimit Data into Tx");
+          tx.print();
+          mas.recive(tx);
+          $display("Master Recived Data");
+          mas.print();
+          $display("Read Transaction Competed");
+          $display("==========================");
+        end
+        if(mas.sel==4'b1000) begin
+          $display("Read Operation {wr_rd = 0}");
+          $display("Slave 4 Selected {sel=4'b1000}");
+          slv4.transmit(tx);
+          $display("Slave Transimit Data into Tx");
+          tx.print();
+          mas.recive(tx);
+          $display("Master Recived Data");
+          mas.print();
+          $display("Read Transaction Competed");
+          $display("==========================");
+        end
+      end
+    end
+  end
+endmodule
---
+++ SV Notes/linux notes.md
@@ -0,0 +1,161 @@
+**ctrl+alt+t :** is the shortcut to open the terminal:
+
+**"root"** directory is the top level directory**.**
+
+
+
+
+**pwd** working directory
+
+**ls   :** shows all files in the directory
+
+**ls-l :** long listing of a directory: info about the files
+
+**ls-ls:**
+
+**ls-F** : classify thre files with adding the character(/) at the end of the file
+
+&nbsp;		"/" includes directory
+
+&nbsp;		"@" includes symbiloc link
+
+&nbsp;		"\*" includes executable files
+
+**ls-a :** to view the Hidden files
+
+**cd   : "change directory**" to go to home directory {cd ~ also}
+
+**cd / :** go back to initial root directory
+
+**cd.. :** goes 1 step back**.**
+
+**cd-  :** to previous step.
+
+**whoami : user** name
+
+**wc filename   :** give line count,word count,character count
+
+**wc-w filename** :  
+
+**head filename :** used to display the top 10 lines of the file
+
+**tail filename** : used to display the bottom 10 lines of the file
+
+**sudo atp install :** 
+
+**suda atp update  :** 
+
+
+
+
+
+
+
+**User interface :**
+
+&nbsp;	it is a window through which the user and computer interact.\*\*
+	there are 2 ways to intract with the system\*\*
+	1. Graphical User Interface.\*\* 
+		here the user uses mouse pointer to interact with the system.\*\*
+	2. Command line interface\*\*
+		here the user uses commands to interact with the system.\*\*
+
+**Directory Structure:**
+
+&nbsp;	it is the top level directories are placed under a top most directory which is called root directory.
+
+
+**list of files in the bin folder:**
+
+
+
+**bin**: it contains binary files
+
+**sbin**: contains super binary files
+
+**boot**: static bootable images
+
+**dev** : device files
+
+**etc** : host specific files
+
+**home**: user home directories
+
+**lib** : essential shared libraries and kernal modules
+
+**media** mount point for removable media
+
+**mnt** : mount point for temporarly mounted file system
+
+**proc**  virtual fs documenting kernal and process status
+
+**root**: root user home directory
+
+**sbin**: essentail super user commands binaries.
+
+**opt** : add on application packages
+
+**tmp** : temporary files 
+
+**usr** : multi user utilites
+
+**var** : variable files ( logs )
+
+
+
+1     2      3    4      5         6                 7                 8                  9                       10
+
+^     ^      ^    ^      ^         ^                 ^                 ^                  ^                        ^
+
+|     |      |    |      |         |                 |                 |                  |                        |
+
+### **drwxr-xr-x  2  emertxe  emertxe  4096  May  17  2020  Desktop**
+
+
+
+**Output field**
+
+
+
+field 1 : 
+
+&nbsp;	it indicates weather it is a file or a folder (there are 7 tpyes of files in linux)
+
+field 2,3,4 :
+
+&nbsp;	indicates the permissons it has read,write,execute
+
+&nbsp;		1.second : user permisson
+
+&nbsp;		2.third  : group permission
+
+&nbsp;		3.fourth : other user permissons
+
+field 5 :
+
+&nbsp;	this feild specifies the numeber of links or directories inside this directory
+
+field 6 :
+
+&nbsp;	the user owns the file or directory.
+
+field 7 :
+
+&nbsp;	the group that the file belongs to, and any user in that group will have permission give in the 3rd feild over that file.
+
+field 8 :
+
+&nbsp;	the size in bytes {4096 bytes}.
+
+feild 9 :
+
+&nbsp;	the date of last modification
+
+feild 10:
+
+&nbsp;	the name of the file
+
+
+
+&nbsp;	
+
