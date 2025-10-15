5c03901731e45fbc0adac8d5c09c74118b84bf85
S-Nikhil-Prince <23a91a0459@aec.edu.in>
2025-10-15 10:15:18 +0530

    ascending and descending number storing in array using constraints

--- Programs/Offline Programs/Programs.sv
+++ Programs/Offline Programs/Programs.sv
@@ -51,3 +51,67 @@ Output:
 # KERNEL: a[7]=13
 # KERNEL: a[8]=9
 # KERNEL: a[9]=1
+
+write a constraint for 4 bit array with size 10 andstore random values into the dynamic array in ascending order.
+class sample;
+  rand bit [3:0]a[];
+  int i,j;
+  constraint c1 {
+    a.size==10;
+    foreach(a[i])
+      if(i>=0)
+        a[i]>a[i-1];
+  }
+endclass
+
+module tb;
+  sample s=new();
+  initial begin
+    assert(s.randomize());
+    foreach (s.a[i])
+      $display("a[%0d]=%0d",i,s.a[i]);
+  end
+endmodule
+output:
+# KERNEL: a[0]=0
+# KERNEL: a[1]=1
+# KERNEL: a[2]=2
+# KERNEL: a[3]=3
+# KERNEL: a[4]=4
+# KERNEL: a[5]=5
+# KERNEL: a[6]=6
+# KERNAL: a[7]=7
+# KERNEL: a[8]=8
+# KERNEL: a[9]=9
+
+write a constraint for 4 bit array with size 10 andstore random values into the dynamic array in ascending order.
+class sample;
+  rand bit [3:0]a[];
+  int i,j;
+  constraint c1 {
+    a.size==10;
+    foreach(a[i])
+      if(i>0)
+        a[i]<a[i-1];
+  }
+endclass
+
+module tb;
+  sample s=new();
+  initial begin
+    assert(s.randomize());
+    foreach (s.a[i])
+      $display("a[%0d]=%0d",i,s.a[i]);
+  end
+endmodule
+output:
+# KERNEL: a[0]=14
+# KERNEL: a[1]=13
+# KERNEL: a[2]=11
+# KERNEL: a[3]=9
+# KERNEL: a[4]=8
+# KERNEL: a[5]=5
+# KERNEL: a[6]=4
+# KERNEL: a[7]=2
+# KERNEL: a[8]=1
+# KERNEL: a[9]=0
