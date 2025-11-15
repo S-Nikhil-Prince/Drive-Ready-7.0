3e948e116856a87a627cd004b441e257a2c06c9d
Nikhil Prince Sodadasi <166209449+S-Nikhil-Prince@users.noreply.github.com>
2025-11-15 14:39:34 +0530

    practice
    Added additional data entry for RTL design specs.

--- Cadence Certificates/demo.py
+++ Cadence Certificates/demo.py
@@ -2,5 +2,8 @@ for i in range (int(input())):
   print(i**)
 # Random data entry 1: VLSI design parameters
 # Clock frequency: 100 MHz
+# Data entry 2: RTL design specs
+# Voltage: 1.8V, Power: 25mW
   
+
 
